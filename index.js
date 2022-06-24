ab1e0c5e258b14ed1bd138fdfb28cf2ba6487445
rathi chandar <rathichandar@gmail.com>
2022-06-24 15:36:09 +0530

    all functions\

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
     const data  = require('C:/Users/ASUS/Desktop/api/data.json');
     const { ifError } = require("assert");
     const { MongoClient, Db } = require("mongodb");
-
+const { Console } = require("console"); 
+var ObjectId = require('mongodb');
+const { json } = require("body-parser");
+mongo = require("mongodb");
     var app =express();
 
 
@@ -27,7 +30,7 @@
 
         if (err) throw err;
         var dbo = db.db("mydb");
-        dbo.collection('customers').find({}).toArray((err,result)=>{
+        dbo.collection('Users').find({}).toArray((err,result)=>{
             if (err) throw err;
             console.log(result);
             db.close()
@@ -54,40 +57,58 @@
     });
 
 
-    //var nameSchema = new mongoose.Schema({
-    //   name: String,
-    // salary: String
-    //});
-    //var User = mongoose.model("User", nameSchema);
+     app.get('/add',(req,res)=>{
+         res.sendFile('C:/Users/ASUS/Desktop/api/index.html')
+     });
+
+     app.post('/add',(req,res)=>{
+         MongoClient.connect(url, function(err, db) {
+             if (err) throw err;
+             var dbo = db.db("mydb");
+
+             var myobj = req.body;
+             dbo.collection("Users").insertOne(myobj, function(err, res) {
+                 if (err) throw err;
+                 console.log("1 document inserted");
+             })
+         })  ;
+     res.send(req.body)
+ });
 
 
-    app.get('/add',(req,res)=>{
-        res.sendFile('C:/Users/ASUS/Desktop/api/index.html')
-    });
 
-    //////////////////////////////////////////////////////////
-    app.post('/add',(req,res)=>{
-        MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db("mydb");
 
-            var myobj = req.body;
-            dbo.collection("Users").insertOne(myobj, function(err, res) {
-                if (err) throw err;
-                console.log("1 document inserted");
+app.delete('/delete/:id',(req,res)=>{
+    var id = req.params.id;
+    MongoClient.connect(url,(err,db)=>{
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("Users").deleteOne(({ _id :  new mongo.ObjectId(id) }),(err,result)=>{
+            if(err) throw err;
+            console.log(result)
+
+        });
+        
+    })
+    res.json({success : id})
+})
+
+app.post('/update',(req,res)=>{
+        
+    MongoClient.connect(url,(err,db)=>{
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        //var id = ObjectId(req.body.id)
+        dbo.collection("Users").findOneAndUpdate({_id : new mongo.ObjectId(req.body.id) },{$set :{name : req.body.name,salary: req.body.salary}}, {new: true}, (err, doc) => {
+            if (err) {
+                console.log("Something wrong when updating data!");
+            }
             
-        
-        
-        //myData.save()
-
-        //.then(item => {
-        //res.send("item saved to database");
-        })
-        //.catch(err => {
-        //res.status(400).send("unable to save to database");
-        })  ;
-        res.send(req.body)
-    });
+            res.json(doc);
+        });
 
 
-    app.listen(3000)
+})
+})
+
+ app.listen(3000)
